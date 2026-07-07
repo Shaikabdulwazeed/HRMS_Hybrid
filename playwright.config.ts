8156f9d489f8f5abf2ecc47976246048187e612b
Shaikabdulwazeed <sabdulwazeed02@gmail.com>
2026-07-07 11:17:02 +0530

    Fix Allure reporter case

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-  ['html'], ['allure-Playwright'],
+  ['list'], 
+  ['allure-playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
